51838b47be2ca06c13bea9be76ed79851e67b87d
Shendi Teuku Maulana Efendi <shendyteuku2@gmail.com>
2025-07-13 20:30:20 +0700

    Update JWT_EXP default value to 60 minutes in AuthConfig

--- backend/src/accounts/config.py
+++ backend/src/accounts/config.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class AuthConfig(BaseSettings):
     JWT_ALG: str
     JWT_SECRET: str
-    JWT_EXP: int = 5  # minutes
+    JWT_EXP: int = 60  # minutes
 
     class Config:
         """Pydantic config."""
